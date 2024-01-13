ee37f91c4a1ed4105477e57531674ad9d23dc76a
Patrick Deutsch <105461352+kcirtapfromspace@users.noreply.github.com>
2024-01-12 17:28:06 -0700

    add back home.stateversion

--- users/thinkstudio/home-manager.nix
+++ users/thinkstudio/home-manager.nix
@@ -17,8 +17,8 @@ let
 in {
   # Home-manager 22.11 requires this be set. We never set it so we have
   # to use the old state version.
-  # home.stateVersion = "18.09";
-
+  home.stateVersion = "23.11";
+  
   xdg.enable = true;
 
   #---------------------------------------------------------------------
